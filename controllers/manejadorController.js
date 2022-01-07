d7817bb097ef8430a49257dbbc09964d9e80daae
tamara96mc <tamara.96mc@gmail.com>
2022-01-07 12:04:31 +0100

    se quita la config de bot

--- controllers/manejadorController.js
+++ controllers/manejadorController.js
@@ -31,81 +31,41 @@ ManejadorController.createBot = (req, res) => {
 
   try {
 
-
     let telefono_manejador = req.body.telefono;
 
     venom
-      .create(
-        //session
-        telefono_manejador, //Pass the name of the client you want to start the bot
-        //catchQR
-        (base64Qrimg, asciiQR, attempts, urlCode) => {
+        //	create bot with options
+        .create(
+          telefono_manejador,
+          (base64Qr, asciiQR, attempts, urlCode) => {
+            console.log(asciiQR); // Optional to log the QR in the terminal
+            var matches = base64Qr.match(/^data:([A-Za-z-+\\/]+);base64,(.+)$/),
+              response = {};
 
-          console.log(asciiQR);
-          console.log('base64 image string qrcode: ', base64Qrimg);
+            if (matches.length !== 3) {
+              return new Error('Invalid input string');
+            }
 
-          try {
-            res.send(base64Qrimg);
-          }
-          catch (err) {
-            res.status(500).send({
-              message:
-                err.message || "Some error occurred while generate qr"
-            });
-          };
+            try {
 
+              res.send(base64Qr);
+            }
+            catch (err) {
+              res.status(500).send({
+                message:
+                  err.message || "Some error occurred while generate qr"
+              });
+            };
 
-        },
-        // statusFind
-        (statusSession, session) => {
-          console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser
-          //Create session wss return "serverClose" case server for close
-          console.log('Session name: ', session);
-        },
-        // options
-        {
-          multidevice: false, // for version not multidevice use false.(default: true)
-          folderNameToken: 'tokens', //folder name when saving tokens
-          mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
-          headless: true, // Headless chrome
-          devtools: false, // Open devtools by default
-          useChrome: true, // If false will use Chromium instance
-          debug: false, // Opens a debug session
-          logQR: true, // Logs QR automatically in terminal
-          browserWS: '', // If u want to use browserWSEndpoint
-          browserArgs: [''], //Original parameters  ---Parameters to be added into the chrome browser instance
-          puppeteerOptions: {}, // Will be passed to puppeteer.launch
-          disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
-          disableWelcome: true, // Will disable the welcoming message which appears in the beginning
-          updatesLog: true, // Logs info updates automatically in terminal
-          autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
-          createPathFileToken: false, // creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
-          chromiumVersion: '818858', // Version of the browser that will be used. Revision strings can be obtained from omahaproxy.appspot.com.
-          addProxy: [''], // Add proxy server exemple : [e1.p.webshare.io:01, e1.p.webshare.io:01]
-          userProxy: '', // Proxy login username
-          userPass: '' // Proxy password
-        },
-        // BrowserSessionToken
-        // To receive the client's token use the function await clinet.getSessionTokenBrowser()
-        // {
-        //   WABrowserId: '"UnXjH....."',
-        //   WASecretBundle:
-        //     '{"key":"+i/nRgWJ....","encKey":"kGdMR5t....","macKey":"+i/nRgW...."}',
-        //   WAToken1: '"0i8...."',
-        //   WAToken2: '"1@lPpzwC...."'
-        // },
-        // BrowserInstance
-        (browser, waPage) => {
-          console.log('Browser PID:', browser.process().pid);
-          waPage.screenshot({ path: 'screenshot.png' });
-        }
-      )
-      .then((client) => {
-        start(client);
-      })
-      .catch((erro) => {
-        console.log(erro);
-      });
+          },
+          undefined,
+          { logQR: true }
+        )
+        .then((client) => start(client))
+        // 	catch errors
+        .catch((err) => {
+          dateLog(err)
+        })
 
 
     const getDataClientes = (nameSesion) => {
